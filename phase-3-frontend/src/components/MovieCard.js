1999184750576ebdaf39ba0f6064dc7ddca3d07d
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-10-21 01:14:16 -0700

    Adds dynamic styling inline to create custom backgrounds with paralax for each movie.

--- phase-3-frontend/src/components/MovieCard.js
+++ phase-3-frontend/src/components/MovieCard.js
@@ -4,13 +4,24 @@ function MovieCard({movie}){
 
   const { id, title, director, year, subgenre, image } = movie;
 
+  const sectionStyle = {
+    width: "100%",
+    height: "600px",
+    backgroundImage: `url(${image})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    backgroundAttachment: "fixed",
+    backgroundPosition: "center"
+  }
+
   return(
     <div className="card" id={id}>
-      <h3>{title}</h3>
-      <iframe src={image} className="movie-image" allowFullScreen title={title}></iframe>
-      <h4>Directed by: {director}</h4> 
+      <div className="background" style={sectionStyle}></div>
+      <h1>{title}</h1>
+      <h3>Directed by: {director}</h3> 
       <p>{title} is a {subgenre} horror movie, originally released in {year}</p>
       <button id="review-button">Add a Review!</button>
+      <div className="background" style={sectionStyle}></div>
     </div>
   )
 }
