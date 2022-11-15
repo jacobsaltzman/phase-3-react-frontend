9178d0522eac7bee26e7bbf703e3a8da317658c1
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-11-14 19:07:42 -0800

    Adds a toggle button for more info

--- phase-3-frontend/src/components/MovieCard.js
+++ phase-3-frontend/src/components/MovieCard.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from 'react';
 
 
 function MovieCard({movie}){
+
+  const [isSeen, setIsSeen] = useState(true);
 
   const { id, title, director, year, subgenre, image } = movie;
 
@@ -15,12 +17,19 @@ function MovieCard({movie}){
     backgroundPosition: "center"
   }
 
+  function handleIsSeen(e){
+    setIsSeen(!isSeen)
+  }
+
   return(
     <div className="card" id={id}>
       <div className="background" style={sectionStyle}></div>
       <h1>{title}</h1>
       <h3>Directed by: {director}</h3> 
       <p>{title} is a {subgenre} horror movie, originally released in {year}</p>
+      <button onClick={handleIsSeen}>{isSeen? 'More Info':'Less info'}</button>
+      <div id='more-info'>
+      </div>
       <div className="background" style={sectionStyle}></div>
     </div>
   )
